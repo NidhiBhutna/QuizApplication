691e4d9e0f7fad71bc1ea12f14e828860c6780da
NidhiBhutna <123177933+NidhiBhutna@users.noreply.github.com>
2023-10-26 10:21:13 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions=[
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool Markup Language",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language",
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        answer: "Cascading Style Sheet",
+        options: [
+            "Common Style Sheet",
+            "Colorful Style Sheet",
+            "Computer Style Sheet",
+            "Cascading Style Sheet",
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does SQL stand for?",
+        answer: "Structured Query Language",
+        options: [
+            "Structured Query Language",
+            "Structured Question Language",
+            "Stylish Query Language",
+            "Stylesheet Query Language",
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does XML stand for?",
+        answer: "eXtensible Markup language",
+        options: [
+            "eXecutable Markup language",
+            "eXtensible Multiple language",
+            "eXtensible Markup language",
+            "eXamine Multiple language",
+        ]
+    },
+    {
+        numb: 5,
+        question: "HTML is the method where ordinary text can be converted into?",
+        answer: "HyperText",
+        options: [
+            "ASCII Text",
+            "HyperText",
+            "EBCDIC Text",
+            "None of the above",
+        ]
+    },
+];
